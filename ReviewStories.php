de770a5d5ad9143f6c03fe5fe6161788124d4db9
Ubuntu <dhc-user@akakur-bravo.novalocal>
2014-12-02 22:10:38 +0000

    Fix for ReviewStories where user id of story was null

--- ReviewStories.php
+++ ReviewStories.php
@@ -12,7 +12,7 @@
 
     $now = date("Y:m:d H:i:s");
 //    $result = mysql_query("SELECT * FROM submissions ORDER BY date DESC LIMIT 10", $link) or die("ERROR: Query failed");
-    $result = $link->query("SELECT * FROM submissions LIMIT 10;") or die("ERROR: Query failed");
+    $result = $link->query("SELECT * FROM submissions LIMIT 10;");
 //    if (mysql_num_rows($result) == 0)
     if (count($result) == 0)
       print("<h2>Sorry, but there is no submitted story available at this time.</h2><br>\n");
@@ -22,8 +22,11 @@
     {
       print("<br><hr>\n");
       printHTMLHighlighted($row["title"]);
-      $username = getUserName($row["writer"], $link);
-      print("<B>Posted by ".$username." on ".$row["date"]."</B><br>\n");
+      if ($row["writer"] == null)
+        $username = "Anonymous Coward";
+      else
+        $username = getUserName($row["writer"], $link);
+      print("<B>Posted by ".$username." on ".date("Y-m-d H:i:s", $row["date"])."</B><br>\n");
       print($row["body"]);
       print("<br><p><center><B>[ <a href=\"/rubbos/AcceptStory.php?storyId=".$row["id"]."\">Accept</a> | <a href=\"/rubbos/RejectStory.php?storyId=".$row["id"]."\">Reject</a> ]</B><p>\n");
     }
